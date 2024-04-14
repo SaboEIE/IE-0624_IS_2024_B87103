e43bf2e8e485c362a73ce1cddb01fcb3e22b7c1d
SaboEIE <kendall.saborio@ucr.ac.cr>
2024-04-13 22:49:25 -0600

    Se añaden algunas variables globales, se modifica un poco el main.
    La modificación en el main se realiza pensando en tener únicamente dos funciones principales, la primera que modifique y configure el micro y la segunda con la lógica de los estados.

--- Laboratorio_02/src/cruce.c
+++ Laboratorio_02/src/cruce.c
@@ -10,12 +10,12 @@
 #include <avr/interrupt.h>
 
 // Definición de los states de la FSM
-#define GO_CAR        0
-#define BLINK_CAR     1
-#define STOP_CAR      2
-#define GO_WALKER     3
-#define BLINK_WALKER  4
-#define STOP_WALKER   5
+#define GO_CAR 0
+#define BLINK_CAR 1
+#define STOP_CAR 2
+#define GO_WALKER 3
+#define BLINK_WALKER 4
+#define STOP_WALKER 5
 
 // Definiciones de tiempos de semáforo
 #define SAFE_TIME 1
@@ -23,14 +23,34 @@
 #define GO_WALKER_TIME 10
 #define ENABLE_PUSH_TIME 10
 
-/* DECLARACIÓN DE FUNCIONES */
+// Estado actual: variable global
+int state;
+
+//
+int seconds;
+int success_ctc = 0;
+
+// Estado de los botones del paso peatonal: variable global
+int btn_pushed;
+
 // Prototipos de funciones
-void setup();
+void init();
 void FSM();
 
-void main(void)
+/* DECLARACIÓN DE FUNCIONES */
+// Prototipos de funciones
+void init();
+void FSM();
+
+/** @brief Función principal.
+ *  @details La función principal solamente hace el llamado de la función
+ *  init() para configurar adecuadamente el ATtiny4313 y FSM() para iniciar con
+ *  las transiciones de estado según sea el caso.
+ */
+int main(void)
 {
-    setup(); // Configuración.
-    sei();   // Habilitación interrupciones.
-    FSM();   // Ejecución de la simulación.
+    init();
+    FSM();
+
+    return 0;
 }
