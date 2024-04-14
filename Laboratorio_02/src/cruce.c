784c038a61d8903966bb8eb6480544208d4cc276
Alexander <ALEXANDER.ROJASBRENES@ucr.ac.cr>
2024-04-13 21:53:07 -0600

    Se agregan las librerías necesarias para la construcción del programa, los defines de la máquina de estados planteada y defines los tiempos establecidos en el diagrama de tiempos del enunciado. Los defines fueron agregados para mejorar la legibilidad del código más adelante.

--- Laboratorio_02/src/cruce.c
+++ Laboratorio_02/src/cruce.c
@@ -5,10 +5,23 @@
  * @date 07/04/2024
  */
 
-/*Librerías*/
+// Librerías necesarias para la ejecición
 #include <avr/io.h>
-#include <util/delay.h>
 #include <avr/interrupt.h>
+
+// Definición de los states de la FSM
+#define GO_CAR        0
+#define BLINK_CAR     1
+#define STOP_CAR      2
+#define GO_WALKER     3
+#define BLINK_WALKER  4
+#define STOP_WALKER   5
+
+// Definiciones de tiempos de semáforo
+#define SAFE_TIME 1
+#define BLINK_TIME 3
+#define GO_WALKER_TIME 10
+#define ENABLE_PUSH_TIME 10
 
 /* DECLARACIÓN DE FUNCIONES */
 // Prototipos de funciones
