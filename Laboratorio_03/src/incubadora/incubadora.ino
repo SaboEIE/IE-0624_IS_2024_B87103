503c8eea28bc756679dac1fec2b0aaae1c8b36cb
SaboEIE <kendall.saborio@ucr.ac.cr>
2024-05-07 22:00:49 -0600

    Construcción del Controlador PID
    Integración del controlador PID al sistema. Se ha añadido la librería PID_v1_bc.h y se ha inicializado el controlador en el modo automático en el setup(). Se han definido las variables y constantes necesarias para el control PID, incluyendo Setpoint, Input, Output, y los coeficientes consKp, consKi, consKd. Además, se ha implementado la computación de la salida del controlador PID en la función loop(), lo que permite ajustar la temperatura de salida según el valor del potenciómetro.

--- Laboratorio_03/src/incubadora/incubadora.ino
+++ Laboratorio_03/src/incubadora/incubadora.ino
@@ -1,6 +1,9 @@
 // Librería para el control del LCD
 #include <PCD8544.h>
 
+// Librería para el control del PID
+#include <PID_v1_bc.h>
+
 // Inicialización de Pines/Variables.
 // Pin de entrada para el potenciómetro
 const int potPin = A0;
@@ -18,8 +21,16 @@ const int red_led = 2;
 const int LCD_PIN = A4;
 
 // Objeto para controlar el LCD
-static PCD8544 lcd; 
+static PCD8544 lcd;
 
+// Variables para el setpoint, la entrada y la salida del PID
+double Setpoint, Input, Output; 
+
+// Constantes para los coeficientes del PID
+double consKp = 255, consKi = 0.0, consKd = 1;
+
+// Variable para el valor convertido de la salida del PID
+float output_coverted;
 
 void setup() {
   // Pin de salida para el LED azul
@@ -37,8 +48,17 @@ void setup() {
   // Inicia la comunicación con el LCD con una resolución predeterminada de 84x48 píxeles
   lcd.begin();
 
-}
+  // Configura el modo de operación del controlador PID como AUTOMÁTICO
+  myPID.SetMode(AUTOMATIC);
 
+}
+/**
+ * @brief Controla la visualización en un LCD de las temperaturas operativa, de salida y la salida del controlador PID.
+ * @details Esta función actualiza la pantalla LCD con las temperaturas operativa, de salida y la salida del controlador PID (Señal de control).
+ * @param op_temp Temperatura operativa actual (en grados Celsius).
+ * @param PID Salida del controlador PID.
+ * @param out_temp Temperatura de salida actual (en grados Celsius).
+ */
 void LCD_control(double op_temp, double PID, double out_temp) {
   // Escribe en la primera línea 
   lcd.setCursor(0 , 0); 
@@ -89,7 +109,44 @@ void LED_control(double out_temp) {
   }
 }
 
+/**
+ * @brief Simula el comportamiento térmico de un bloque de aluminio con calentamiento y enfriamiento pasivo.
+ * @details Esta función calcula la temperatura del bloque de aluminio en función del tiempo, considerando la transferencia de calor
+ * desde un calentador y el enfriamiento por convección al ambiente.
+ * @param Q Cantidad de calor suministrado al bloque de aluminio (en Julios por segundo).
+ * @return T La temperatura actual del bloque de aluminio (en grados Celsius).
+ */
+float simPlanta(float Q){
+  // simula un bloque de aluminio de 1x1x2cm con un calentador y con enfreamiento pasivo
+  float h = 5; // W/m2K coeficiente de conveccion termica para el Aluminio
+  float Cps = 0.89; // J/gC
+  float area = 1e-4; // m2 area por conveccion
+  float masa = 10 ; // g
+  float Tamb = 25; // Temperatura ambiente en C
+  static float T = Tamb; // Temperatura en C
+  static uint32_t last = 0;
+  uint32_t interval = 100; // ms
+  
+  if ( millis () - last >= interval) {
+    last += interval;
+    // 0−transferencia de calor
+    T = T + Q * interval / 1000 / masa / Cps - (T - Tamb) *   area * h;
+  }
 
+  return T;
+}
+/**
+ * @brief Función principal del programa que se ejecuta repetidamente.
+ * @details Esta función realiza las siguientes operaciones en un ciclo continuo:
+ * 1. Lee el valor del potenciómetro.
+ * 2. Mapea el valor leído a un rango de 20 a 80.
+ * 3. Actualiza el valor de referencia del controlador PID (Setpoint).
+ * 4. Calcula la salida del controlador PID.
+ * 5. Convierte la salida en temperatura del controlador PID a watts.
+ * 6. Simula el comportamiento térmico de la planta y obtiene la temperatura de salida.
+ * 7. Controla los LEDs en función de la temperatura de salida.
+ * 8. Actualiza la pantalla LCD si el pin de control está en estado alto.
+ */
 void loop() {
   // Lee el valor del potenciómetro
   int potValue = analogRead(potPin);
@@ -100,13 +157,22 @@ void loop() {
   // Actualiza el valor de referencia del controlador PID (Setpoint)
   Setpoint = potmappedValue;
 
+  // Calcula la salida del controlador PID
+  myPID.Compute();
+
+  // Convierte la salida en temperatura del controlador PID a watts
+  output_coverted = Output * 20.0 / 255;
+
+  // Simula el comportamiento térmico de la planta y obtiene la temperatura de salida
+  Input = simPlanta(output_coverted);
+
   // Función que controla los LED a partir de la temperatura sensada. 
   LED_control(Input);
 
   // Actualiza la pantalla LCD si el pin de control está en estado alto
   if (digitalRead(LCD_PIN) == HIGH) {
     // Alternar el estado de la pantalla LCD
-    //LCD_control(pote, Output, Input);
+    LCD_control(potmappedValue, Output, Input);
   }
   else {
     // Limpiar la pantalla LCD si el pin de control está en estado bajo
