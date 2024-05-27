8b2558a9cae7fa5484492fdf8645aad21adf3e2f
SaboEIE <kendall.saborio@ucr.ac.cr>
2024-05-27 13:55:57 -0600

    Se añade función setup para configurar conexión serial y cliente MQTT
    Se Implementa la función `setup` que configura la comunicación serial con el dispositivo a través del puerto `/dev/ttyACM0` con una tasa de baud de 115200. Crea y configura un cliente MQTT, inicializando la bandera de conexión del cliente MQTT. Establece la función de devolución de llamada `on_connect` para manejar la conexión al broker MQTT. Configura el broker MQTT con la dirección "iot.eie.ucr.ac.cr" y el puerto 1883. Define el tema MQTT "v1/devices/me/telemetry" para el envío de datos. Establece el identificador del dispositivo MQTT para la autenticación. Conecta el cliente MQTT al broker y crea un diccionario para almacenar los datos a enviar. Finalmente retorna los objetos y parámetros configurados: comunicación serial, diccionario, cliente MQTT y tema MQTT.

--- Laboratorio_4/src/iot.py
+++ Laboratorio_4/src/iot.py
@@ -44,3 +44,34 @@ def conn_check(client):
     while client.connected != True:  # Mientras el cliente MQTT no esté conectado
         client.loop()  # Procesar mensajes entrantes y salientes
         time.sleep(2)  # Esperar 2 segundos antes de realizar la siguiente verificación
+        
+def setup():
+    """
+    Configura la conexión serial, el cliente MQTT y otros parámetros necesarios.
+
+    Esta función configura la comunicación serial con el dispositivo,
+    crea un cliente MQTT, establece los parámetros de conexión al broker MQTT,
+    y define un diccionario para almacenar los datos a enviar.
+
+    Returns:
+        tuple: Una tupla que contiene los siguientes elementos en orden:
+               - datos: El objeto serial para la comunicación con el dispositivo.
+               - diccionario: Un diccionario para almacenar los datos a enviar.
+               - client: El cliente MQTT configurado y conectado al broker.
+               - topic: El tema MQTT al que se enviarán los datos.
+    """
+    datos = serial.Serial("/dev/ttyACM0", 115200, timeout=1)  # Configurar la comunicación serial con el dispositivo
+    client = mqtt.Client()  # Crear un cliente MQTT
+    client.connected = False  # Inicializar la bandera de conexión del cliente MQTT
+    client.on_connect = on_connect  # Establecer la función de devolución de llamada para la conexión al broker MQTT
+
+    broker = "iot.eie.ucr.ac.cr"  # Dirección del broker MQTT
+    port = 1883  # Puerto del broker MQTT
+    topic = "v1/devices/me/telemetry"  # Tema MQTT al que se enviarán los datos
+    device = "q2IvRYzJf7zHTMSR0PKU"  # Identificador del dispositivo MQTT
+    client.username_pw_set(device)  # Establecer el identificador del dispositivo como usuario para la conexión al broker MQTT
+    client.connect(broker, port)  # Conectar el cliente MQTT al broker MQTT
+
+    diccionario = dict()  # Crear un diccionario para almacenar los datos a enviar
+
+    return datos, diccionario, client, topic  # Retornar los objetos y parámetros configurados
